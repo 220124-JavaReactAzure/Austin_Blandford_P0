0be27aecc60e36ed466549b9cda8090a0d795e0a
Austin Blandford <ablandford2399@gmail.com>
2022-01-28 14:38:31 -0800

    Updated BankDriver to print information and take user input. Driver interacts with newly created Customer class and prints the information the user input to the console.

--- ABlandford_P0_BankingApp/src/com/revature/bank_app/BankDriver.java
+++ ABlandford_P0_BankingApp/src/com/revature/bank_app/BankDriver.java
@@ -1,9 +1,35 @@
 package com.revature.bank_app;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import com.revature.bank_app.models.Customer;
+
 public class BankDriver {
 
-	public static void main(String[] args) {
-		System.out.println("Hello Bank!");
+	static BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
+	
+	public static void main(String[] args) throws IOException {
+		
+		System.out.println("Welcome to your new banking app! To get started, we will need some basic information from you.");
+		System.out.println("Please enter the information below.");
+		
+		System.out.print("First Name: ");
+		String firstName = consoleReader.readLine();
+		
+		System.out.print("Last Name: ");
+		String lastName = consoleReader.readLine();
+		
+		System.out.print("Email: ");
+		String email = consoleReader.readLine();
+		
+		System.out.print("Password: ");
+		String password = consoleReader.readLine();
+		
+		Customer customer = new Customer(firstName, lastName, email, password);
+		System.out.println("The information you entered is below.");
+		System.out.print(customer.toString());
 	}
 
 }
---
+++ ABlandford_P0_BankingApp/src/com/revature/bank_app/models/Customer.java
@@ -0,0 +1,90 @@
+package com.revature.bank_app.models;
+
+public class Customer {
+
+	// Variables
+	private String customerId;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String password;
+	private String accountId;
+	
+	// Constructor
+	public Customer() {
+		super();
+	}
+	
+	public Customer(String firstName, String lastName, String email, String password) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.password = password;
+	}
+
+	public Customer(String customerId, String firstName, String lastName, String email, String password, String accountId) {
+		super();
+		this.customerId = customerId;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.password = password;
+		this.accountId = accountId;
+	}
+
+	// Getters and Setters
+	public String getCustomerId() {
+		return customerId;
+	}
+
+	public void setCustomerId(String customerId) {
+		this.customerId = customerId;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getAccountId() {
+		return accountId;
+	}
+
+	public void setAccountId(String accountId) {
+		this.accountId = accountId;
+	}
+	
+	@Override
+	public String toString() {
+		return "Customer [Customer Id: " + customerId + ", First Name: " + firstName + ", Last Name: " + lastName + ", Email: " + email + ", Password: " + password + ", Account Id: " + accountId + "]";
+	}
+	
+}
